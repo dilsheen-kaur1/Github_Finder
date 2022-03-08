9de92935786af109f4d148c005afed4f5ec154f3
dilsheen-kaur1 <dilsheen.kaur@tothenew.com>
2022-03-08 15:51:42 +0530

    users display using context API

---
+++ src/components/users/UserResults.jsx
@@ -0,0 +1,41 @@
+import {useContext, useEffect,useState} from 'react'
+import Spinner from '../layouts/Spinner';
+import UserItem from './UserItem';
+import GithubContext from '../../context/github/GithubContext';
+
+function UserResults() {
+    // const [users, setUsers] = useState([]);
+    // const [loading, setLoading] = useState(true)//false default
+    const { users, loading ,fetchUsers } = useContext(GithubContext)
+    useEffect(()=>{
+        fetchUsers()
+    },[])
+
+    // const fetchUsers = async ()=>{
+    //     const response = await fetch(`https://api.github.com/users`, {
+    //         headers:{
+    //             Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
+    //         }
+    //     })
+    //     const data = await response.json();
+    //     setUsers(data)
+    //     setLoading(false)
+    // }
+
+    if(!loading){
+        return (
+            <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3'>
+                {users.map((user) =>(
+                    // <h3>{user.login}</h3>
+                    <UserItem key={user.id} user={user} />
+                ))}
+            </div>
+          )
+    }
+    else{
+        return <Spinner />
+    }
+ 
+}
+
+export default UserResults
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import UserResults from "../components/users/UserResults"
+
+function Home() {
+  return (
+    <>
+        {/* Seacrch component */}
+        <UserResults />
+        
+    </>
+  )
+}
+
+export default Home
